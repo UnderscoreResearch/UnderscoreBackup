2bd3cc5b046a86ac2eb377a9b9f0d9a83db86469
Henrik Johnson <spam@henrik.org>
2023-05-14 21:22:57 -0700

    Updated Choco packages

--- windows/Choco/tools/chocolateyinstall.ps1
+++ windows/Choco/tools/chocolateyinstall.ps1
@@ -1,24 +1,17 @@
-﻿$ErrorActionPreference = 'Stop' # stop on all errors
+﻿$ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64      = 'https://github.com/UnderscoreResearch/UnderscoreBackup/releases/download/2.0.2.3/underscorebackup-2.0.4.0.exe' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
+$url64      = 'https://github.com/UnderscoreResearch/UnderscoreBackup/releases/download/2.0.4.1/underscorebackup-2.0.4.1.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  fileType      = 'exe' #only one of these: exe, msi, msu
+  fileType      = 'exe'
   url64bit      = $url64
-  #file         = $fileLocation
 
-  softwareName  = 'Underscore Backup' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
-
-  # Checksums are now required as of 0.10.0.
-  # To determine checksums, you can get that from the original site if provided.
-  # You can also use checksum.exe (choco install checksum) and use it
-  # e.g. checksum -t sha256 -f path\to\file
+  softwareName  = 'Underscore Backup'
   checksum64    = 'TBD'
-  checksumType64= 'sha256' #default is checksumType
+  checksumType64= 'sha256'
 
-  # MSI
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
   validExitCodes= @(0)
 }
